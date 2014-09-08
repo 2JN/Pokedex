d9b1b22143733acf71f4ba92773fda59d6a452e6
2J-N <jesquivelnavas@gmail.com>
2014-09-08 15:29:24 -0600

    Pushing files

---
+++ pokemon.js
@@ -0,0 +1,54 @@
+function Pokemon(n,e,t)
+{
+	this.nombre = n;
+	this.especie = e;
+	this.tipo = t;
+}
+
+function charizard()
+{
+	var charizard = new Pokemon("Charizard","Flama","Fuego Volador");
+	NPokemon.textContent = charizard.nombre;
+	EPokemon.textContent = charizard.especie;
+	TPokemon.textContent = charizard.tipo;
+}
+
+function furret()
+{
+	var furret = new Pokemon("Furret","Cuerpo Alargado","Normal");
+	NPokemon.textContent = furret.nombre;
+	EPokemon.textContent = furret.especie;
+	TPokemon.textContent = furret.tipo;
+}
+
+function typhlosion()
+{
+	var typhlosion = new Pokemon("Typhlosion","Volcan","Fuego")
+	NPokemon.textContent = typhlosion.nombre;
+	EPokemon.textContent = typhlosion.especie;
+	TPokemon.textContent = typhlosion.tipo
+}
+
+function banette()
+{
+	var banette = new Pokemon("Banette","Marioneta","Fantasma");
+	NPokemon.textContent = banette.nombre;
+	EPokemon.textContent = banette.especie;
+	TPokemon.textContent = banette.tipo;
+}
+
+function zoroark()
+{
+	var zoroark = new Pokemon("Zoroark","Zorro Ilusion","Oscuro");
+	NPokemon.textContent = zoroark.nombre;
+	EPokemon.textContent = zoroark.especie;
+	TPokemon.textContent = zoroark.tipo;
+}
+
+function swampert()
+{
+	var swampert = new Pokemon("Swampert","Pez Lodo","Agua Tierra");
+	NPokemon.textContent = swampert.nombre;
+	EPokemon.textContent = swampert.especie;
+	TPokemon.textContent = swampert.tipo;
+}
